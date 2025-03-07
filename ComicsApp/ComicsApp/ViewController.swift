c7652841e51384e4a6645464fee8fd090c24b194
duartecsilva <duarte.silva@plexus.tech>
2025-03-07 15:46:19 +0000

    Revert "Paginainicialelistapkmn"

--- ComicsApp/ComicsApp/ViewController.swift
+++ ComicsApp/ComicsApp/ViewController.swift
@@ -8,36 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    let pokedexVC = PokeDexMainScreenViewController()
-    let MarvelVC = MarvelMainScreenViewController()
-    let RMVC = RickAndMortyMainScreenViewController()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
-    
-    @IBAction func PokeButton(_ sender: UIButton) {
-        if let navigationController = self.navigationController {
-            //            let pokedexVC = PokeDexMainScreenViewController()
-            navigationController.pushViewController(pokedexVC, animated: true)
-        }
-        
-    }
-    
-    @IBAction func MarvelButton(_ sender: UIButton) {
-        //        present(MarvelVC, animated: true, completion: nil)
-        if let navigationController = self.navigationController {
-            navigationController.pushViewController(MarvelVC, animated: true)
-        }
-        
-    }
-    
-    @IBAction func RMButton(_ sender: UIButton) {
-        //        present(RMVC, animated: true, completion: nil)
-        if let navigationController = self.navigationController {
-            navigationController.pushViewController(RMVC, animated: true)
-        }
-    }
-    
+
+
 }
 
--- ComicsApp/Pods/Target Support Files/Pods-ComicsApp-ComicsAppUITests/Pods-ComicsApp-ComicsAppUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ComicsApp_ComicsAppUITests : NSObject
-@end
-@implementation PodsDummy_Pods_ComicsApp_ComicsAppUITests
-@end
